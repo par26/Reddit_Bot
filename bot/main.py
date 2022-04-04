65e1787d72730557c7241ea1399dea7400f52f44
Kingston Du <kdu08@live.com>
2022-04-04 15:12:42 -0700

    added discord bot

---
+++ bot/main.py
@@ -0,0 +1,30 @@
+import discord
+import time
+import os
+
+
+infoFile = open("../fakeData.txt")
+data = infoFile.read()
+running = False
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        self.running = False
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content == "~break":
+            self.running = False
+
+        if message.content == "~start":
+            self.running = True
+            while self.running:
+                await message.channel.send('**' + data + '**')
+                time.sleep(5)
+
+client = MyClient()
+
+client.run('OTYwNjQ4MTY5ODYyMDk4OTY2.YktfUw.HvT3sMWtKC1BI8wHuMyEsUTc4uA')
