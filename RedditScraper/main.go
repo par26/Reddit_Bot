a785bf3d096a8227990ad3a0e23c1738f42c8372
Pratush Agrawal <67389873+rocketmind26@users.noreply.github.com>
2022-04-06 21:02:36 -0700

    Update main.go

--- RedditScraper/main.go
+++ RedditScraper/main.go
@@ -1,1 +1,48 @@
+package main
 
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	questions, err := GetPost("Top", "Showerthoughts", 20)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writeToFile(questions)
+}
+
+func writeToFile(questions []string) {
+	f, err := os.Create("question.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file
+	defer f.Close()
+
+	for _, question := range questions {
+		_, err := f.WriteString(question + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+/*unc getUserInput() {
+
+	var input string
+
+	fmt.Scanln(input)
+
+	switch input {
+	case "1":
+
+	default:
+		fmt.Println("Invalid Input")
+	}
+
+	fmt.Println("Uh oh")
+}*/
