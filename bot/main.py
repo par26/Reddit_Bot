34250fe7e06e1424826db2dbd690702231c6672c
Kingston Du <kdu08@live.com>
2022-04-18 18:32:41 -0700

    yes

--- bot/main.py
+++ bot/main.py
@@ -2,15 +2,17 @@ import discord
 import time
 import os
 
-
+os.system(os.getcwd() + "\main")
+print(os.getcwd() + "\..\main")
 infoFile = open("../fakeData.txt")
-data = infoFile.read()
+data = infoFile.readlines()
 running = False
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
         self.running = False
+        self.counter = 0
 
     async def on_message(self, message):
         if message.author == client.user:
@@ -21,10 +23,13 @@ class MyClient(discord.Client):
 
         if message.content == "~start":
             self.running = True
+
             while self.running:
-                await message.channel.send('**' + data + '**')
-                time.sleep(5)
+                await message.channel.send('**' + data[self.counter] + '**')
+                self.counter += 1
+                if self.running:
+                    time.sleep(5)
 
 client = MyClient()
 
-client.run('OTYwNjQ4MTY5ODYyMDk4OTY2.YktfUw.HvT3sMWtKC1BI8wHuMyEsUTc4uA')
+client.run('OTYwNjQ4MTY5ODYyMDk4OTY2.YktfUw.Zb6NolVQsWIAsJXj2OaNmbYqu2w')
